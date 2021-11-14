501f766f1593451212d66f1fbe836051ecdaf2bc
Perkanttechh <tech.master@perkanttech.com>
2021-11-14 18:54:03 +0530

    sunshine

--- app/screens/CheckListScreen.js
+++ app/screens/CheckListScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, TouchableOpacity, View, Text, Button } from 'react-native';
 import AppHeading from '../components/AppHeading';
 import Screen from '../components/Screen';
@@ -23,18 +23,18 @@ const CheckListScreen = ({ navigation }) => {
 
     const onChange = (event, selectedDate) => {
         const currentDate = moment(selectedDate).format("DD/MM/YYYY") ?? moment(date).format("DD/MM/YYYY");
-      setShow(Platform.OS === 'ios');
+        setShow(Platform.OS === 'ios');
         setDate(selectedDate ?? moment(date).format("DD/MM/YYYY"));
         setGivenOnDate(currentDate);
     };
-  
+
     const showMode = (currentMode) => {
-      setShow(true);
-      setMode(currentMode);
+        setShow(true);
+        setMode(currentMode);
     };
-  
+
     const showDatepicker = () => {
-      showMode('date');
+        showMode('date');
     };
 
 
@@ -47,157 +47,159 @@ const CheckListScreen = ({ navigation }) => {
                 <View style={styles.selectFlex}>
                     <AppHeading>Select Your Vaccination</AppHeading>
                     <View>
-                <View>
+                        <View>
 
-                <TouchableOpacity
-                activeOpacity={0.8}
-                onPress={showDatepicker}
-                style={styles.datePicker}
-                >
-                    <Text style={styles.selectedDate}>{moment(date).format("DD/MM/YYYY")}</Text>
-                    <FontAwesome5 name="calendar-alt" size={24} color="#515151" />
-                </TouchableOpacity>
-                </View>
-                    {show && (
-                        <DateTimePicker
-                            testID="dateTimePicker"
-                            value={date}
-                            mode={date}
-                            is24Hour={true}
-                            display="default"
-                            onChange={onChange}
-                        />
-                    )}
-                </View>
+                            <TouchableOpacity
+                                activeOpacity={0.8}
+                                onPress={showDatepicker}
+                                style={styles.datePicker}
+                            >
+                                <Text style={styles.selectedDate}>{moment(date).format("DD/MM/YYYY")}</Text>
+                                <FontAwesome5 name="calendar-alt" size={24} color="#515151" />
+                            </TouchableOpacity>
+                        </View>
+                        {show && (
+                            <DateTimePicker
+                                testID="dateTimePicker"
+                                value={date}
+                                mode={date}
+                                is24Hour={true}
+                                display="default"
+                                onChange={onChange}
+                            />
+                        )}
+                    </View>
                 </View>
                 <View style={styles.cont}>
                     <View style={styles.checkboxContain}>
-                            <View style={styles.radio}>
-                                <RadioButton
-                                    value="Birth"
+                        <View style={styles.radio}>
+                            <RadioButton
+                                value="Birth"
                                 status={checked === 0 ? 'checked' : 'unchecked'}
-                                    color="#79D1D7"
+                                color="#79D1D7"
                                 onPress={() => setChecked(0)}
-                                />
-                                <CardPara>Birth</CardPara>
-                            </View>
-                            <View style={styles.radio}>
-                                <RadioButton
-                                    value="6 weeks"
+                            />
+                            <CardPara>Birth</CardPara>
+                        </View>
+                        <View style={styles.radio}>
+                            <RadioButton
+                                value="6 weeks"
                                 status={checked === 6 ? 'checked' : 'unchecked'}
-                                    color="#79D1D7"
+                                color="#79D1D7"
                                 onPress={() => setChecked(6)}
-                                />
-                                <CardPara>6 weeks</CardPara>
-                            </View>
-                            <View style={styles.radio}>
-                                <RadioButton
-                                    value="10 weeks"
+                            />
+                            <CardPara>6 weeks</CardPara>
+                        </View>
+                        <View style={styles.radio}>
+                            <RadioButton
+                                value="10 weeks"
                                 status={checked === 10 ? 'checked' : 'unchecked'}
-                                    color="#79D1D7"
+                                color="#79D1D7"
                                 onPress={() => setChecked(10)}
-                                />
-                                <CardPara>10 weeks</CardPara>
-                            </View>
-                            <View style={styles.radio}>
-                                <RadioButton
-                                    value="14 weeks"
+                            />
+                            <CardPara>10 weeks</CardPara>
+                        </View>
+                        <View style={styles.radio}>
+                            <RadioButton
+                                value="14 weeks"
                                 status={checked === 14 ? 'checked' : 'unchecked'}
-                                    color="#79D1D7"
+                                color="#79D1D7"
                                 onPress={() => setChecked(14)}
-                                />
-                                <CardPara>14 weeks</CardPara>
-                            </View>
-                            <View style={styles.radio}>
-                                <RadioButton
-                                    value="6 months"
+                            />
+                            <CardPara>14 weeks</CardPara>
+                        </View>
+                        <View style={styles.radio}>
+                            <RadioButton
+                                value="6 months"
                                 status={checked === 24 ? 'checked' : 'unchecked'}
-                                    color="#79D1D7"
+                                color="#79D1D7"
                                 onPress={() => setChecked(24)}
-                                />
-                                <CardPara>6 months</CardPara>
-                            </View>
-                            <View style={styles.radio}>
-                                <RadioButton
+                            />
+                            <CardPara>6 months</CardPara>
+                        </View>
+                        <View style={styles.radio}>
+                            <RadioButton
                                 value="7 months"
-                                status={checked === 36 ? 'checked' : 'unchecked'}
-                                    color="#79D1D7"
-                                onPress={() => setChecked(36)}
-                                />
+                                status={checked === 28 ? 'checked' : 'unchecked'}
+                                color="#79D1D7"
+                                onPress={() => setChecked(28)}
+                            />
                             <CardPara>7 months</CardPara>
-                            </View>
-                            <View style={styles.radio}>
-                                <RadioButton
+                        </View>
+                        <View style={styles.radio}>
+                            <RadioButton
                                 value="6-9 months"
-                                status={checked === 40 ? 'checked' : 'unchecked'}
-                                    color="#79D1D7"
-                                onPress={() => setChecked(40)}
-                                />
+                                status={checked === 32 ? 'checked' : 'unchecked'}
+                                color="#79D1D7"
+                                onPress={() => setChecked(32)}
+                            />
                             <CardPara>6-9 months</CardPara>
-                            </View>
+                        </View>
                     </View>
                     <View style={styles.checkboxContain}>
-                            <View style={styles.radio}>
-                                <RadioButton
-                                    value="9 months"
-                                status={checked === 48 ? 'checked' : 'unchecked'}
-                                    color="#79D1D7"
-                                onPress={() => setChecked(48)}
-                                />
-                                <CardPara>9 months</CardPara>
-                            </View>
-                            <View style={styles.radio}>
-                                <RadioButton
-                                    value="12 months"
-                                status={checked === 60 ? 'checked' : 'unchecked'}
-                                    color="#79D1D7"
-                                onPress={() => setChecked(60)}
-                                />
-                                <CardPara>12 months</CardPara>
-                            </View>
-                            <View style={styles.radio}>
-                                <RadioButton
-                                    value="15 months"
-                                status={checked === 64 ? 'checked' : 'unchecked'}
+                        <View style={styles.radio}>
+                            <RadioButton
+                                value="9 months"
+                                status={checked === 36 ? 'checked' : 'unchecked'}
                                 color="#79D1D7"
-                                onPress={() => setChecked(64)}
+                                onPress={() => setChecked(36)}
+                            />
+                            <CardPara>9 months</CardPara>
+                        </View>
+                        <View style={styles.radio}>
+                            <RadioButton
+                                value="12 months"
+                                status={checked === 48 ? 'checked' : 'unchecked'}
+                                color="#79D1D7"
+                                onPress={() => setChecked(48)}
+                            />
+                            <CardPara>12 months</CardPara>
+                        </View>
+                        <View style={styles.radio}>
+                            <RadioButton
+                                value="15 months"
+                                status={checked === 60 ? 'checked' : 'unchecked'}
+                                color="#79D1D7"
+                                onPress={() => setChecked(60)}
                             />
                             <CardPara>15 months</CardPara>
                         </View>
                         <View style={styles.radio}>
                             <RadioButton
                                 value="18 months"
-                                status={checked === 72 ? 'checked' : 'unchecked'}
+                                status={checked === 64 ? 'checked' : 'unchecked'}
                                 color="#79D1D7"
-                                onPress={() => setChecked(72)}
+                                onPress={() => setChecked(64)}
                             />
                             <CardPara>18 months</CardPara>
                         </View>
                         <View style={styles.radio}>
                             <RadioButton
-                                    value="18-19 months"
-                                status={checked === 96 ? 'checked' : 'unchecked'}
-                                    color="#79D1D7"
-                                onPress={() => setChecked(96)}
-                                />
-                                <CardPara>18-19 months</CardPara>
-                            </View>
-                            <View style={styles.radio}>
-                            <RadioButton
-                                    color="#79D1D7"
-                                onPress={() => setChecked(192)}
-                                />
-                            <CardPara>4-6 years</CardPara>
-                            </View>
+                                value="18-19 months"
+                                status={checked === 72 ? 'checked' : 'unchecked'}
+                                color="#79D1D7"
+                                onPress={() => setChecked(72)}
+                            />
+                            <CardPara>18-19 months</CardPara>
+                        </View>
                         <View style={styles.radio}>
-                                <RadioButton
-                                    value="10 years"
+                            <RadioButton
+                                value="4-6 years"
+                                status={checked === 192 ? 'checked' : 'unchecked'}
+                                color="#79D1D7"
+                                onPress={() => setChecked(192)}
+                            />
+                            <CardPara>4-6 years</CardPara>
+                        </View>
+                        <View style={styles.radio}>
+                            <RadioButton
+                                value="10 years"
                                 status={checked === 480 ? 'checked' : 'unchecked'}
-                                    color="#79D1D7"
+                                color="#79D1D7"
                                 onPress={() => setChecked(480)}
-                                />
+                            />
                             <CardPara>10-12 years</CardPara>
-                            </View>
+                        </View>
                     </View>
                 </View>
                 <AppButton
@@ -240,17 +242,17 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center'
     },
-    datePicker:{
-        width:'55%',
+    datePicker: {
+        width: '55%',
         borderRadius: 5,
         backgroundColor: '#f4f4f4',
-        display:'flex',
+        display: 'flex',
         flexDirection: 'row',
-        justifyContent:'space-between',
-        paddingHorizontal:12,
-        paddingVertical:12,
+        justifyContent: 'space-between',
+        paddingHorizontal: 12,
+        paddingVertical: 12,
     },
-    selectedDate:{
+    selectedDate: {
         fontFamily: 'PublicSans-Light',
         color: colors.textGrey
     }
